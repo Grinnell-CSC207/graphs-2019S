8b2f92e653cbf3a97931a569abe0ddd2b84caa7f
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2019-05-03 08:50:29 -0500

    Minor code cleanup + use validVertex.

--- src/Graph.java
+++ src/Graph.java
@@ -22,11 +22,11 @@ public class Graph {
   // +-------+
 
   /*
-   * We implement our graphs using adjacency lists.  For each
-   * vertex, v, we store a list of edges from that vertex.
+   * We implement our graphs using adjacency lists. For each vertex, v, we store
+   * a list of edges from that vertex.
    *
-   * For convenience, you can refer to vertices by number or by
-   * name.  However, it is more efficient to refer to them by number.
+   * For convenience, you can refer to vertices by number or by name. However,
+   * it is more efficient to refer to them by number.
    */
 
   // +-----------+---------------------------------------------------
@@ -53,14 +53,14 @@ public class Graph {
   int numEdges;
 
   /**
-   * The vertices in the graph.  The edges from vertex v are
-   * stored in vertices[v].
+   * The vertices in the graph. The edges from vertex v are stored in
+   * vertices[v].
    */
   List<Edge>[] vertices;
 
   /**
-   * The names of the vertices.  The name of vertex v is stored
-   * in vertexNames[v].
+   * The names of the vertices. The name of vertex v is stored in
+   * vertexNames[v].
    */
   String[] vertexNames;
 
@@ -70,14 +70,14 @@ public class Graph {
   Queue<Integer> unusedVertices;
 
   /**
-   * The numbers of the vertices.  The vertex with name n is in
+   * The numbers of the vertices. The vertex with name n is in
    * vertexNumbers.get(n).
    */
-  HashMap<String,Integer> vertexNumbers;
+  HashMap<String, Integer> vertexNumbers;
 
   /**
-   * The version of the graph.  (Essentially, the number of times
-   * we've modified the graph.)
+   * The version of the graph. (Essentially, the number of times we've modified
+   * the graph.)
    */
   long version;
 
@@ -99,7 +99,7 @@ public class Graph {
   public Graph(int initialCapacity) {
     this.vertices = (ArrayList<Edge>[]) new ArrayList[initialCapacity];
     this.vertexNames = new String[initialCapacity];
-    this.vertexNumbers = new HashMap<String,Integer>();
+    this.vertexNumbers = new HashMap<String, Integer>();
     this.unusedVertices = new LinkedList<Integer>();
     this.version = 0;
     for (int i = 0; i < this.vertices.length; i++) {
@@ -113,11 +113,11 @@ public class Graph {
   // +----------------------+
 
   /**
-   * Given a vertex number, get the corresponding vertex name.
-   * If there is no corresponding vertex name, returns null;
+   * Given a vertex number, get the corresponding vertex name. If there is no
+   * corresponding vertex name, returns null;
    */
   public String vertexName(int vertexNumber) {
-    if ((vertexNumber < 0) || (vertexNumber >= vertexNames.length)) {
+    if (!validVertex(vertexNumber)) {
       return null;
     } else {
       return this.vertexNames[vertexNumber];
@@ -125,8 +125,8 @@ public class Graph {
   } // vertexName(int)
 
   /**
-   * Given a vertex name, get the corresponding vertex number.
-   * If there is no corresponding vertex number, returns -1.
+   * Given a vertex name, get the corresponding vertex number. If there is no
+   * corresponding vertex number, returns -1.
    */
   public int vertexNumber(String vertexName) {
     Integer result = vertexNumbers.get(vertexName);
@@ -159,7 +159,7 @@ public class Graph {
     } // for
     pen.println();
   } // dump()
-  
+
   /**
    * Get the number of edges.
    */
@@ -179,7 +179,7 @@ public class Graph {
    */
   public Iterator<Edge> edges() {
     return new Iterator<Edge>() {
-      // The position of the iterator 
+      // The position of the iterator
       int pos = 0;
       // The version number of the graph when this iterator was created
       long version = Graph.this.version;
@@ -216,13 +216,15 @@ public class Graph {
    * Get all of the edges from a particular vertex.
    */
   public Iterator<Edge> edgesFrom(int vertex) {
-    if ((vertex < 0) || (vertex >= this.vertices.length)) {
+    if (!validVertex(vertex)) {
       return new Iterator<Edge>() {
         long version = Graph.this.version;
+
         public boolean hasNext() {
           failFast(this.version);
           return false;
         } // hasNext()
+
         public Edge next() {
           failFast(this.version);
           throw new NoSuchElementException();
@@ -232,7 +234,7 @@ public class Graph {
       return new Iterator<Edge>() {
         // The version number of the graph when this iterator was created
         long version = Graph.this.version;
-        // The underlying iterator.  We wrap it so that the client
+        // The underlying iterator. We wrap it so that the client
         // cannot call the remove method.
         Iterator<Edge> edges = Graph.this.vertices[vertex].iterator();
 
@@ -296,8 +298,8 @@ public class Graph {
   // +----------+
 
   /**
-   * Add an edge between two vertices.  If the edge already exists,
-   * replace it.  If the vertices are invalid, throws an exception.
+   * Add an edge between two vertices. If the edge already exists, replace it.
+   * If the vertices are invalid, throws an exception.
    */
   public void addEdge(int from, int to, int weight) throws Exception {
     if (!validVertex(from) || !validVertex(to)) {
@@ -320,8 +322,8 @@ public class Graph {
   } // addEdge(int, int, int)
 
   /**
-   * Add an edge between two vertices.  If the edge already exists,
-   * replace it.  if the vertices are invalid, throws an exception.
+   * Add an edge between two vertices. If the edge already exists, replace it.
+   * if the vertices are invalid, throws an exception.
    */
   public void addEdge(String from, String to, int weight) throws Exception {
     addEdge(this.vertexNumber(from), this.vertexNumber(to), weight);
@@ -358,7 +360,7 @@ public class Graph {
   } // addVertex()
 
   /**
-   * Remove an edge.  If the edge does not exist, does nothing.
+   * Remove an edge. If the edge does not exist, does nothing.
    */
   public void removeEdge(int from, int to) {
     Iterator<Edge> ie = this.vertices[from].iterator();
@@ -374,14 +376,14 @@ public class Graph {
   } // removeEdge(int, int)
 
   /**
-   * Remove an edge.  If the edge does not exist, does nothing.
+   * Remove an edge. If the edge does not exist, does nothing.
    */
   public void removeEdge(String from, String to) {
     removeEdge(this.vertexNumber(from), this.vertexNumber(to));
   } // removeEdge(String, String)
 
   /**
-   * Remove a vertex.  If the vertex does not exist, does nothing.
+   * Remove a vertex. If the vertex does not exist, does nothing.
    */
   public void removeVertex(int vertex) {
     // Ignore pointless vertex numbers
@@ -414,7 +416,7 @@ public class Graph {
   } // removeVertex(int)
 
   /**
-   * Remove a vertex.  If the vertex does not exist, does nothing.
+   * Remove a vertex. If the vertex does not exist, does nothing.
    */
   public void removeVertex(String vertex) {
     this.removeVertex(this.vertexNumber(vertex));
@@ -423,7 +425,7 @@ public class Graph {
   // +-----------+---------------------------------------------------
   // | Utilities |
   // +-----------+
- 
+
   /**
    * Add a vertex name / vertex number pair.
    *
@@ -436,7 +438,7 @@ public class Graph {
     this.vertexNames[v] = name;
     return v;
   } // addVertex(String, int)
-    
+
   /**
    * Expand the necessary arrays.
    */
@@ -452,9 +454,9 @@ public class Graph {
   } // expand()
 
   /**
-   * Compare an expected version to the current version.  Die
-   * if they do not match.  (Used to implement the traditional
-   * "fail fast" policy for iterators.)
+   * Compare an expected version to the current version. Die if they do not
+   * match. (Used to implement the traditional "fail fast" policy for
+   * iterators.)
    */
   private void failFast(long expectedVersion) {
     if (this.version != expectedVersion) {
@@ -466,8 +468,8 @@ public class Graph {
    * Determine if a vertex is valid.
    */
   private boolean validVertex(int vertex) {
-    return ((vertex >= 0) && (vertex < this.vertices.length) &&
-        (this.vertexNames[vertex] != null));
+    return ((vertex >= 0) && (vertex < this.vertices.length)
+        && (this.vertexNames[vertex] != null));
   } // validVertex
 
   /**
