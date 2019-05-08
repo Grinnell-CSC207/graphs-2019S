9f60fdf39325e3beb3d8b26c298b4298c5e197b6
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2019-05-07 19:42:04 -0500

    Better overriding.

--- src/UndirectedGraph.java
+++ src/UndirectedGraph.java
@@ -4,17 +4,37 @@
  * @author Samuel A. Rebelsky
  */
 public class UndirectedGraph extends Graph {
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Create a basic undirected graph.
+   */
+  public UndirectedGraph() {
+    super();
+  } // UndirectedGraph()
+
+  /**
+   * Create a basic undirected graph from a file.
+   */
+  public UndirectedGraph(String fname) throws Exception {
+    super(fname);
+  } // UndirectedGraph(fname)
+
   // +-----------+---------------------------------------------------
   // | Overrides |
   // +-----------+
 
   @Override
   public void addEdge(String u, String v, int weight) throws Exception {
-    int unum = this.safeVertexNumber(u);
-    int vnum = this.safeVertexNumber(v);
-    super.addEdge(unum, vnum, weight);
-    super.addEdge(vnum, unum, weight);
+    addEdge(this.safeVertexNumber(u), this.safeVertexNumber(v), weight);
   } // addEdge(String, String, int)
+  
+  public void addEdge(int u, int v, int weight) throws Exception {
+    super.addEdge(u, v, weight);
+    super.addEdge(v, u, weight);
+  } // addEdge(int, int, int)
 
   @Override
   public void removeEdge(String u, String v) {
